891d3a6c1da036d2b5fbc3356755befdf931d0a1
Fabio <fabiofreelancerrecife@gmail.com>
2026-04-19 11:31:32 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,136 @@
+/* Config Sample
+ *
+ * For more information on how you can configure this file
+ * see https://docs.magicmirror.builders/configuration/introduction.html
+ * and https://docs.magicmirror.builders/modules/configuration.html
+ *
+ * You can use environment variables using a `config.js.template` file instead of `config.js`
+ * which will be converted to `config.js` while starting. For more information
+ * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
+ */
+let config = {
+	address: "localhost",	// Address to listen on, can be:
+							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+							// - another specific IPv4/6 to listen on a specific interface
+							// - "0.0.0.0", "::" to listen on any interface
+							// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
+	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+									// you must set the sub path here. basePath must end with a /
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
+									// or add a specific IPv4 of 192.168.1.5 :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "pt",
+	locale: "pt-BR",   // this variable is provided as a consistent location
+			   // it is currently only used by 3rd party modules. no MagicMirror code uses this value
+			   // as we have no usage, we  have no constraints on what this field holds
+			   // see https://en.wikipedia.org/wiki/Locale_(computer_software) for the possibilities
+
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	timeFormat: 24,
+	units: "metric",
+
+	modules: [
+		{
+			module: "alert",
+		},
+
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+		    module: "MMM-Remote-Control",
+		    position: "bottom_left",
+		    config: {
+			customCommand: {},
+			showModuleApiMenu: true,
+			secureEndpoints: true,
+		    }
+		},
+		{
+			module: "clock",
+			position: "top_left",
+			config: {
+			displayType: "analog",
+			analogSize: "250px",
+			analogFace: "face-001"
+			},
+		},
+		{
+			module: 'MMM-WeeksCalendar',
+			position: "top_right",
+			header: "",
+			config: {
+			updateDelay: 5,
+			showAdjacentMonths: true
+			}
+		},
+		{
+			module: "compliments",
+			position: "middle_center",
+			// position: "lower_third",
+			config: {
+				remoteFile: 'https://raw.githubusercontent.com/gantzfabio/Compliments/refs/heads/main/Compliments.json',
+				classes: "xlarge",
+				updateInterval: "4000",
+				fadeSpeed: "2500",
+				
+				// See 'Configuration options' for more information.
+			      },
+			
+		},
+		{
+			module: "weather",
+			position: "top_right",
+			config: {
+				weatherProvider: "openmeteo",
+				type: "current",
+				lat: -7.834103,
+				lon: -34.906061
+			}
+		},
+		{
+			module: "weather",
+			position: "top_right",
+			header: "",
+			config: {
+				weatherProvider: "openmeteo",
+				type: "forecast",
+				lat: -7.834103,
+				lon: -34.906061
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "G1 - Todas as notícias",
+						url: "https://g1.globo.com/dynamo/rss2.xml"
+					},
+					{
+						title: "Carros (Autoesporte.com)",
+						url: "https://g1.globo.com/dynamo/carros/rss2.xml"
+					}
+				],
+				wrapDescription: false,
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") { module.exports = config; }
